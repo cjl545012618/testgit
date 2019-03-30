da9c041247e116806bc02d241aef6fe18eaef391
cjl545012618 <545012618@qq.com>
2019-03-30 14:49:39 +0800

    把本地工程提交到git上

---
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @author Cjl
+ * 2019/3/30 11:42
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+    }
+
+}
